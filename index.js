176c44c4c2d5aaee972e905013745e25bad7ae29
Himiko <128669887+SakuraHimiko@users.noreply.github.com>
2024-04-27 23:15:33 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+const authUName = "SakuraHimiko"
+const authPass = "mikumiku"
+const username = prompt("Username: ")
+const password = prompt("Password: ")
+if(authUName === username && authPass === password){
+  alert("Siccessfully Logged In")
+}else{
+  alert("Wrong Username or Password")
+}
+
